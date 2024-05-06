8748794fe05e91428db642d2d27e4528af335ff0
ThangLD <leducthang1991@gmail.com>
2024-05-06 08:45:03 +0700

    chỉnh lại bộ Id

--- UnitConverter/Unit.cs
+++ UnitConverter/Unit.cs
@@ -41,35 +41,35 @@ namespace UnitConverter
             return Id == mys.Id;
         }
 
-        public readonly static Unit m = new Unit(150, "m", "Meter", UnitType.LENGTH.Id, 1, null, 1  );
-        public readonly static Unit dam = new Unit(151, "dam", "Decameter", UnitType.LENGTH.Id, Math.Pow(10, 1));
-        public readonly static Unit hm = new Unit(152, "hm", "Hectometer", UnitType.LENGTH.Id, Math.Pow(10, 2));
-        public readonly static Unit km = new Unit(153, "km", "Kilometer", UnitType.LENGTH.Id, Math.Pow(10, 3));
-        public readonly static Unit dm = new Unit(149, "dm", "Decimeter", UnitType.LENGTH.Id, Math.Pow(10, -1));
-        public readonly static Unit cm = new Unit(148, "cm", "Centimeter", UnitType.LENGTH.Id, Math.Pow(10, -2));
-        public readonly static Unit mm = new Unit(147, "mm", "Milimeter", UnitType.LENGTH.Id, Math.Pow(10, -3));
+        public readonly static Unit s = new Unit(UnitType.TIME.Id * 100 + 50, "s", "Second", UnitType.TIME.Id, 1, null, 1);
+        public readonly static Unit ms = new Unit(s.Id - 3, "ms", "Milisecond", UnitType.TIME.Id, 0.001);
+        public readonly static Unit min = new Unit(s.Id - 1, "min", "Minute", UnitType.TIME.Id, 60);
 
-        public readonly static Unit kg = new Unit(250, "kg", "Kilogram ", UnitType.MASS.Id, 1, null, 1);
-        public readonly static Unit yen = new Unit(250, "yen", "yen ", UnitType.MASS.Id, 10);
-        public readonly static Unit cwt = new Unit(252, "cwt", "quintal", UnitType.MASS.Id, 100);
-        public readonly static Unit ton = new Unit(253, "ton", "ton", UnitType.MASS.Id, 1000);
-        public readonly static Unit hg = new Unit(249, "dam", "Hectogram", UnitType.MASS.Id, 0.1);
-        public readonly static Unit dag = new Unit(248, "hm", "Decagram", UnitType.MASS.Id, 0.01);
-        public readonly static Unit g = new Unit(247, "g", "gram", UnitType.MASS.Id, 0.001);
-        public readonly static Unit dg = new Unit(246, "dg", "decigram ", UnitType.MASS.Id, 0.0001);
-        public readonly static Unit cg = new Unit(245, "cg", "centigram", UnitType.MASS.Id, 0.00001);
-        public readonly static Unit mg = new Unit(244, "mg", "Miligram", UnitType.MASS.Id, 0.000001);
+        public readonly static Unit m = new Unit(UnitType.LENGTH.Id * 100 + 50, "m", "Meter", UnitType.LENGTH.Id, 1, null, 1);
+        public readonly static Unit dam = new Unit(m.Id + 1, "dam", "Decameter", UnitType.LENGTH.Id, Math.Pow(10, 1));
+        public readonly static Unit hm = new Unit(m.Id + 2, "hm", "Hectometer", UnitType.LENGTH.Id, Math.Pow(10, 2));
+        public readonly static Unit km = new Unit(m.Id + 3, "km", "Kilometer", UnitType.LENGTH.Id, Math.Pow(10, 3));
+        public readonly static Unit dm = new Unit(m.Id - 1, "dm", "Decimeter", UnitType.LENGTH.Id, Math.Pow(10, -1));
+        public readonly static Unit cm = new Unit(m.Id - 2, "cm", "Centimeter", UnitType.LENGTH.Id, Math.Pow(10, -2));
+        public readonly static Unit mm = new Unit(m.Id - 3, "mm", "Milimeter", UnitType.LENGTH.Id, Math.Pow(10, -3));
+
+        public readonly static Unit kg = new Unit(UnitType.MASS.Id * 100 + 50, "kg", "Kilogram ", UnitType.MASS.Id, 1, null, 1);
+        public readonly static Unit yen = new Unit(kg.Id + 1, "yen", "yen ", UnitType.MASS.Id, 10);
+        public readonly static Unit cwt = new Unit(kg.Id + 2, "cwt", "quintal", UnitType.MASS.Id, 100);
+        public readonly static Unit ton = new Unit(kg.Id + 3, "ton", "ton", UnitType.MASS.Id, 1000);
+        public readonly static Unit hg = new Unit(kg.Id - 1, "dam", "Hectogram", UnitType.MASS.Id, 0.1);
+        public readonly static Unit dag = new Unit(kg.Id - 2, "hm", "Decagram", UnitType.MASS.Id, 0.01);
+        public readonly static Unit g = new Unit(kg.Id - 3, "g", "gram", UnitType.MASS.Id, 0.001);
+        public readonly static Unit dg = new Unit(kg.Id - 4, "dg", "decigram ", UnitType.MASS.Id, 0.0001);
+        public readonly static Unit cg = new Unit(kg.Id - 5, "cg", "centigram", UnitType.MASS.Id, 0.00001);
+        public readonly static Unit mg = new Unit(kg.Id - 6, "mg", "Miligram", UnitType.MASS.Id, 0.000001);
 
 
-        public readonly static Unit s = new Unit(350, "s", "Second", UnitType.TIME.Id, 1, null, 1);
-        public readonly static Unit ms = new Unit(347, "ms", "Milisecond", UnitType.TIME.Id, 0.001);
-        public readonly static Unit min = new Unit(351, "min", "Minute", UnitType.TIME.Id, 60);
+        public readonly static Unit N = new Unit(UnitType.FORCE.Id * 100 + 50, "N", "Newton", UnitType.FORCE.Id, 1, new Dictionary<Unit, double> { { kg, 1 }, { m, 1 }, { s, -2 } }, 0.101972);
+        public readonly static Unit kN = new Unit(N.Id + 3, "kN", "Newton", UnitType.FORCE.Id, Math.Pow(10, 3));
 
-        public readonly static Unit N = new Unit(450, "N", "Newton", UnitType.FORCE.Id, 1, new Dictionary<Unit, double> { { kg, 1 }, { m, 1 }, { s, -2 } }, 0.101972);
-        public readonly static Unit kN = new Unit(453, "kN", "Newton", UnitType.FORCE.Id, Math.Pow(10, 3));
-
-        public readonly static Unit Pa = new Unit(550, "Pa", "Pascal", UnitType.PRESSURE.Id, 1, new Dictionary<Unit, double> { { kg, 1 }, { m, -1 }, { s, -2 } }, 0.101972);
-        public readonly static Unit MPa= new Unit(553, "MPa", "Mega Pascal", UnitType.PRESSURE.Id, Math.Pow(10, 3));
+        public readonly static Unit Pa = new Unit(UnitType.PRESSURE.Id * 100 + 50, "Pa", "Pascal", UnitType.PRESSURE.Id, 1, new Dictionary<Unit, double> { { kg, 1 }, { m, -1 }, { s, -2 } }, 0.101972);
+        public readonly static Unit MPa = new Unit(Pa.Id + 3, "MPa", "Mega Pascal", UnitType.PRESSURE.Id, Math.Pow(10, 3));
 
         public readonly static List<Unit> ListEnum = new List<Unit>()
         {
--- UnitConverter/UnitType.cs
+++ UnitConverter/UnitType.cs
@@ -16,9 +16,14 @@ namespace UnitConverter
             this.Name = name;
         }
 
-        public readonly static UnitType LENGTH = new UnitType(1, "Length");
-        public readonly static UnitType MASS = new UnitType(2, "Mass");
-        public readonly static UnitType TIME = new UnitType(3, "Time");
+        public readonly static UnitType TIME = new UnitType(1, "Time");
+        public readonly static UnitType LENGTH = new UnitType(2, "Length");
+        public readonly static UnitType MASS = new UnitType(3, "Mass");
+        public readonly static UnitType ELECTRIC_CURRENT = new UnitType(4, "Electric current");
+        public readonly static UnitType TEMPERATURE = new UnitType(5, "Temperature");
+        public readonly static UnitType AMOUNT_SUBSTANCE = new UnitType(6, "Amount of substance");
+        public readonly static UnitType LUMINOUS_INTENSITY = new UnitType(7, "luminous intensity");
+       
         public readonly static UnitType FORCE = new UnitType(4, "Force");
         public readonly static UnitType PRESSURE = new UnitType(5, "Pressure");
         public readonly static UnitType ENERGY = new UnitType(6, "Energy");
